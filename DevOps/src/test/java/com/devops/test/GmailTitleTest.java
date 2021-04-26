c17fb66e47cf987f67686becbcfbff4ebba1f102
NitheshHS <66508499+NitheshHS@users.noreply.github.com>
2021-04-26 12:31:07 +0530

    my first commit

---
+++ DevOps/src/test/java/com/devops/test/GmailTitleTest.java
@@ -0,0 +1,24 @@
+package com.devops.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GmailTitleTest {
+	
+	@Test
+	public void test() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://www.google.com/");
+		System.out.println(driver.getTitle());
+		driver.quit();
+	}
+
+}
